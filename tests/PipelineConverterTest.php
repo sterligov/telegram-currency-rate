83a5844b7a5f2832527e15bb75b893c32bd41d1b
Denis Sterligov <denis0324@gmail.com>
2020-02-11 19:40:27 +0300

    add tests for pipeline converter

---
+++ tests/PipelineConverterTest.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Tests;
+
+
+use App\Currency\PipelineConverter;
+use App\Currency\ConverterInterface;
+use App\Exception\CurrencyConverterException;
+use PHPUnit\Framework\TestCase;
+
+class PipelineConverterTest extends TestCase
+{
+    public function testConverter()
+    {
+        $rate = 2;
+        $exception = new CurrencyConverterException();
+
+        $converter1 = $this->createMock(ConverterInterface::class);
+        $converter1->method('convert')
+            ->willThrowException($exception);
+
+        $converter2 = $this->createMock(ConverterInterface::class);
+        $converter2->method('convert')
+            ->willReturn($rate);
+
+        $pipeline = new PipelineConverter([
+            $converter1,
+            $converter2
+        ]);
+
+        $this->assertEquals($rate, $pipeline->convert(978, 643));
+    }
+
+    public function testConverterException()
+    {
+        $exception = new CurrencyConverterException();
+
+        $converter = $this->createMock(ConverterInterface::class);
+        $converter->method('convert')
+            ->willThrowException($exception);
+
+        $pipeline = new PipelineConverter([$converter]);
+
+        $this->expectException(CurrencyConverterException::class);
+        $pipeline->convert(978, 643);
+
+    }
+}
