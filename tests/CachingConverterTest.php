eae1c1d73f13d531c1e6702bd3e054d6cbe84969
Denis Sterligov <denis0324@gmail.com>
2020-04-17 16:03:43 +0500

    add test for caching converter

---
+++ tests/CachingConverterTest.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Tests;
+
+
+use App\Currency\CachingConverter;
+use App\Currency\Currency;
+use App\Currency\RussianCentralBank;
+use PHPUnit\Framework\TestCase;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
+
+class CachingConverterTest extends TestCase
+{
+    public function testConvertWithCache()
+    {
+        $expectedRate = 2;
+        $cacheMock = $this->cacheMock(true, $expectedRate);
+        $converterMock = $this->createMock(RussianCentralBank::class);
+        $converter = new CachingConverter($converterMock, $cacheMock);
+        $rate = $converter->convert(new Currency(643), new Currency(978));
+
+        $this->assertEquals($expectedRate, $rate);
+    }
+
+    private function cacheMock($isHit, $rate)
+    {
+        $cacheItem = $this->createMock(CacheItemInterface::class);
+        $cacheItem->method('isHit')
+            ->willReturn($isHit);
+        $cacheItem->method('get')
+            ->willReturn($rate);
+
+        $cache = $this->createMock(CacheItemPoolInterface::class);
+        $cache->method('getItem')
+            ->willReturn($cacheItem);
+
+        return $cache;
+    }
+}
